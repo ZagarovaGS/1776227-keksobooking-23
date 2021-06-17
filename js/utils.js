8ae93306d6030c8ef60a42f1fee1d4078a411276
n459sverk@gmail.com <n459sverk@gmail.com>
2021-06-17 13:47:17 +0300

    разбили объекты недвижимости по модулям. Первоначальный вариант

---
+++ js/utils.js
@@ -0,0 +1,30 @@
+const isPositiveNumber = (value) => typeof value === 'number' && value >= 0;
+
+const getRandomBetween = (min, max, dec) => {
+  if (
+    !isPositiveNumber(min) ||
+    !isPositiveNumber(max) ||
+    !isPositiveNumber(dec)
+  ) {
+    throw new Error('Неверный тип аргументов');
+  }
+
+  const pow = Math.pow(10, dec);
+  const result = Math.round((Math.random() * (max - min) + min) * pow) / pow;
+
+  return result;
+};
+
+const getRandomBoolean = () => Math.random() >= 0.5;
+
+const getRandomItems = (array, canBeEmpty = true) => {
+  const result = array.filter(getRandomBoolean);
+  if (!canBeEmpty && result.length < 1) {
+    result.push(array[Math.floor(Math.random() * array.length)]);
+  }
+  return result;
+};
+
+const getRandomItem = (array) => array[getRandomBetween(0, array.length, 0)];
+
+export { getRandomBetween, getRandomItems, getRandomItem };
